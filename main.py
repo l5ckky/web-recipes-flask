65aaa56ce36651e43cf4345dec29a445241d25db
Ivan <luckytv211209@gmail.com>
2025-05-01 23:01:14 +0900

    испрвления

--- main.py
+++ main.py
@@ -7,6 +7,56 @@ import api
 
 app = Flask(__name__)
 
+available_ingredients = [{'category': 'Овощи',
+                         'ingredients': [
+                             ('Картофель', 'Картошка'),
+                             'Морковь',
+                             'Лук',
+                             'Помидоры',
+                             'Огурцы',
+                             'Баклажаны',
+                         ]},
+                         {'category': 'Мясо и птица',
+                          'ingredients': [
+                              'Курица',
+                              'Говядина',
+                              'Свинина',
+                              'Баранина',
+                          ]},
+                         {'category': 'Рыба',
+                          'ingredients': [
+                              'weg',
+                              'Мrjtyrtyорковь',
+                              'Лwhtук',
+                              'Помиherдоры',
+                          ]},
+                         {'category': 'Молочные продукты',
+                          'ingredients': [
+                              'Молоко',
+                              'Сыр',
+                              'Творог',
+                              'Йогурт'
+                          ]},
+                         {'category': 'Фрукты и ягоды',
+                          'ingredients': [
+                              'Яблоки',
+                              'Бананы',
+                              'Мандарины',
+                              'Апельсины',
+                              'Вишня',
+                              'Виноград',
+                          ]},
+                         {'category': 'ауауц',
+                          'ingredients': [
+                              ('Картацауццофель', 'Картошка'),
+                              'цуп',
+                              'Лупкупкук',
+                              'Помипккпкдоры',
+                              'к',
+                              'Бакпкпйлажаны',
+                          ]},
+
+                         ]
 
 # base = 'https://www.russianfood.com'
 
@@ -14,21 +64,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', ingredients=available_ingredients)
 
 
 @app.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
-        ingredients = list(filter(lambda x: x[0], request.values.listvalues()))[0]
-        print(ingredients)
-        # if not ingredients:
-        #     return jsonify({'recipes': []})
+        data = list(filter(lambda x: x[0], request.values.listvalues()))
+        if len(data) > 0:
+            ingredients = data[0]
+        else:
+            ingredients = []
 
         response = api.scrape_recipes(ingredients)
         return render_template('index.html',
                                recipes=response['recipes'],
-                               msg=response['msg'])
+                               msg=response['msg'],
+                               ingredients=available_ingredients)
     elif request.method == 'GET':
         return "error"
 
